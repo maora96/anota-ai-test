65b20374b983e8941a9a8ea4208a4efe6b763516
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-02-22 20:56:07 -0300

    feat: filter products by title or category

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -4,9 +4,11 @@ import Product from "../models/product.js";
 export const createProduct = async (req, res) => {
   const product = req.body;
 
+  // create new product from request
   const newProduct = new Product(product);
 
   try {
+    // save new product to database
     await newProduct.save();
     res.status(201).json(newProduct);
   } catch (error) {
@@ -15,9 +17,27 @@ export const createProduct = async (req, res) => {
 };
 
 export const getProducts = async (req, res) => {
+  const { title = null, category = null } = req.query;
   try {
-    const products = await Product.find();
-    res.status(200).json(products);
+    if (title && category === null) {
+      // if only title is provided to filter products
+      const products = await Product.find({ title: title });
+      res.status(200).json(products);
+    } else if (category && title === null) {
+      // if only category is provided to filter products
+      const products = await Product.find({ category: category });
+      res.status(200).json(products);
+    } else if (category && title) {
+      // if both category and title are provided to filter products
+      const products = await Product.find({
+        $and: [{ title: title }, { category: category }],
+      });
+      res.status(200).json(products);
+    } else {
+      // if no title or category is provided to filter products (get all products)
+      const products = await Product.find();
+      res.status(200).json(products);
+    }
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -27,6 +47,7 @@ export const updateProduct = async (req, res) => {
   const { id } = req.params;
   const { title, description, category, price } = req.body;
 
+  // check if there is a prodcut with this id
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send(`No post with id: ${id}`);
   }
@@ -39,6 +60,7 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
+  // check if there is a prodcut with this id
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send(`No post with id: ${id}`);
   }
